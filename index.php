d3f658a5ee88180b5f6830752bdcd5850cf2999e
Stephan Roloff <sr@werbeagenten.de>
2022-05-30 08:54:58 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+ 
+/*
+Plugin Name: Guten Config
+Description: Gutenberg Configuration.
+Version 1.0
+Author: Stephan
+*/
+ 
+if(! defined('ABSPATH')) exit; //Exit if accessed directly
+ 
+// function noDynamic(){
+//    wp_enqueue_script('carga-no-dinamico', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-element', 'wp-editor', 'wp-blocks'));
+//    wp_enqueue_style('carga-no-dinamico-styles', plugin_dir_url(__FILE__) . 'build/index.css');
+// }
+ 
+// add_action('init', 'noDynamic');
+ 
+// include 'src/blocks/are-you-paying-attention/are-you-paying-attention.php';
