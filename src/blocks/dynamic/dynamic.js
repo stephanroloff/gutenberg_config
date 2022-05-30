0c6470e83673d206a699072c384ecf6e4b859615
Stephan Roloff <sr@werbeagenten.de>
2022-05-30 10:26:01 +0200

    Server Side Render in JS

--- src/blocks/dynamic/dynamic.js
+++ src/blocks/dynamic/dynamic.js
@@ -1,12 +1,24 @@
 import "../../index.scss";
 import { registerBlockType } from "@wordpress/blocks";
+import ServerSideRender from "@wordpress/server-side-render";
 
 registerBlockType("ourplugin/dynamic", {
   title: "Dynamic Block",
   icon: "smiley",
   category: "common",
   edit: function () {
-    return <h3 className="text-h3">This is a DINAMIC BLOCK in the Editor!</h3>;
+    return (
+      <div>
+        <h3 className="text-h3">This is a DINAMIC BLOCK in the Editor!!!</h3>
+        <ServerSideRender
+          block="ourplugin/dynamic"
+          attributes={{
+            showPostCounts: true,
+            displayAsDropdown: false,
+          }}
+        />
+      </div>
+    );
   },
   save: function () {
     return null;
